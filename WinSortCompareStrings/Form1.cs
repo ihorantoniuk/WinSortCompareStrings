bf461fe422f23c9063311819c110c613b175fab1
ihorantoniuk <igorantonyuk@gmail.com>
2023-11-25 18:31:54 +0100

    Add project files.

---
+++ WinSortCompareStrings/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinSortCompareStrings
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] array = textBox1.Text.Split(',');
+            MyCompare<string> myCompare = new MyCompare<string>();
+            Array.Sort(array, myCompare);
+
+            foreach (string str in array)
+            {
+                textBox2.Text += str + ",";
+            }
+            textBox2.Text = textBox2.Text.Remove(textBox2.Text.Length - 1);
+        }
+        class MyCompare<T> : IComparer<T>
+        {
+            int IComparer<T>.Compare(T x, T y)
+            {
+                string a = x as string;//"Ivanov Petya - 5"
+                String b = y as String;
+                int score_x = int.Parse((a.Split('-'))[1]);
+                int score_y = int.Parse((b.Split('-'))[1]);
+
+                return (score_x > score_y) ? -1 : 1;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2(textBox2.Text);
+            form2.ShowDialog(this);
+            textBox2.Text = form2.strMsg;
+        }
+    }
+}
---
+++ WinSortCompareStrings/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinSortCompareStrings
+{
+    public partial class Form2 : Form
+    {
+        public string strMsg;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public Form2(string init)
+        {
+            strMsg = init;
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show(strMsg);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = strMsg;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            strMsg = textBox1.Text;
+        }
+    }
+}
